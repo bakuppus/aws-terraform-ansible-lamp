2220fd9c62f4766fa9371311ec562bb5403c4721
root <root@ip-172-31-86-163.ec2.internal>
2018-07-03 19:03:50 +0000

    main.tf elb added

--- main.tf
+++ main.tf
@@ -106,3 +106,37 @@ resource "aws_security_group" "lamp-sec-grp" {
 
 }
 
+resource "aws_elb" "web" {
+name = "lamp-elb"
+
+subnets = ["subnet-94a51bdf"]
+
+security_groups = ["${aws_security_group.lamp-sec-grp.id}"]
+
+listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  # The instance is registered automatically
+
+  instances                   = ["${aws_instance.ec2-lamp.id}"]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+}
+
+
+
+
