7b4c7fc1a15c13e90b7a6656d08afbb71d803ed6
bakuppus <31225958+bakuppus@users.noreply.github.com>
2018-07-05 22:14:45 +0530

    added comments

--- vars.tf
+++ vars.tf
@@ -5,17 +5,18 @@ variable "name" {
   default = "server"
 }
 
-
+# Instance type 
 variable "instance_type" {
   description = "AWS instance type to launch lamp server"
   default = "t2.micro"
 }
 
-
+# private key 
 variable "aws_key_path" {
   default = "/home/ec2-user/lamp.pem" 
 }
 
+# Keypair to login as ec2-user
 variable "key_pair" {
   description = "Name of key pair to attach to the  lamp server instance."
   default = "devec2-keypair"
